162b3b82c169cef42e6b92241aab7a6b43f06647
pengfugui <2360296017@qq.com>
2018-01-04 17:00:55 +0800

    fix report hour

--- src/Brookin/MarketingAPI/Report/HourlyReportModel.php
+++ src/Brookin/MarketingAPI/Report/HourlyReportModel.php
@@ -10,4 +10,22 @@ class HourlyReportModel extends BaseReportModel
      * @var string
      */
     protected $hour;
+
+    /**
+     * @return string
+     */
+    public function getHour()
+    {
+        return $this->hour;
+    }
+
+    /**
+     * @param string $hour
+     */
+    public function setHour($hour)
+    {
+        $this->hour = $hour;
+    }
+
+
 }
